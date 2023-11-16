8e2657d79b35a39a985ef0e152ee1bb4af9912b9
Caleb Eden <ceden0207@yahoo.com>
2023-11-16 17:07:50 -0600

    add mention of pytest in comment

--- manual_tests/serial_stdin.dir/sbin/init.c
+++ manual_tests/serial_stdin.dir/sbin/init.c
@@ -2,9 +2,9 @@
 
 /**
  * This is just a simple program that allows us to test that we can read from serial input
- * I did not put this with the unit_tests since it requires user input and validation
- * If someone wants to figure out a way to automate this (shell scripts?) feel free
-*/
+ * Can be automated with the pytest in manual_tests/test_run_manuals.py
+ *   (looks at manual_tests/serial_stdin.dir/files/sample_input.txt)
+ */
 int main(int argc, char** argv) {
     char str[256] = {};
     printf("%s\n", gets(str));
