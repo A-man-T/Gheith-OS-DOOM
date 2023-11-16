edd512c5154d11cc982844c869979b0e1ae700a5
starship006 <thisiscodyr@yahoo.com>
2023-11-16 00:07:38 -0600

    remove unecessary constant for now

--- kernel/syscalls.h
+++ kernel/syscalls.h
@@ -2,8 +2,6 @@
 #include "file_descriptor.h"
 
 // Define constants
-// TODO: remove hardcodes when libc is finished
-const int HARDCODED_OCLOEXEC = 524288;
 const int HARDCODED_SEEK_SET = 0;
 const int HARDCODED_SEEK_CUR = 1;
 const int HARDCODED_SEEK_END = 2;
