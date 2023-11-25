abb214947ba22316f3fd1325dc43ab64949887d9
Caleb Eden <ceden0207@yahoo.com>
2023-11-25 00:03:03 -0600

    Implement execve in libc

--- libc/newlib/src/newlib/libc/sys/gheithos/syscalls.c
+++ libc/newlib/src/newlib/libc/sys/gheithos/syscalls.c
@@ -14,7 +14,7 @@
 #define SYS_SHUTDOWN 7
 #define SYS_YIELD 998
 #define SYS_JOIN 999
-#define SYS_EXECL 1000
+#define SYS_EXECVE 1000
 #define SYS_KILL 1027
 #define SYS_SEM 1001
 #define SYS_UP 1002
@@ -175,19 +175,9 @@ int close(int file) {
 
 char *environ[] = {0};
 int _execve(const char *name, const char **argv, const char **env) {
-    // TODO remove fixed upper limit once execve added
     // TODO environment?
 
-    const char *args[100];
-    args[0] = name;
-    for (int i = 0; i < 99; i++) {
-        args[i + 1] = argv[i];
-        if (argv[i] == 0) {
-            return setErrno(__syscall_varargs(SYS_EXECL, (const void **)args));
-        }
-    }
-    errno = E2BIG;
-    return -1;
+    return setErrno(__syscall3(SYS_EXECVE, name, argv, env));
 }
 
 int fork() {
